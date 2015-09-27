acb424c9cf763327a34a328ae570fa908bb29174
Thembani Fubesi <tfubesi@live.com>
2015-09-27 21:48:44 +0200

    add directive, controller, filter and service tests

--- app/main.js
+++ app/main.js
@@ -1,1 +1,35 @@
-angular.module("addressBook",[]);
+angular.module("addressBook",[])
+    .service("contactService", function($http){
+        var self = this;
+        self.contacts = [];
+
+        $http.get("http://localhost:9001/contacts").then(function(res){
+            while(res.data[0]){
+                self.contacts.push(res.data.pop());
+            }
+        });
+    })
+    .controller("contactController", function(contactService, $scope){
+
+        $scope.contacts = contactService.contacts;
+
+    })
+    .filter("proper", function(){
+        return function(name){
+            var type = typeof name;
+            if(type !== "string" && type !== "number") throw new Error();
+            return name.toString().split(" ").map(function(word){
+                return word[0].toUpperCase().concat(word.slice(1));
+            }).join(" ");
+        };
+    })
+    .directive("avatar", function(){
+       return{
+            restrict:"AE",
+            scope:{
+                name:"="
+            },
+           template:"<span class=\"avatar\">{{name[0] | proper}}</span>"
+
+       };
+    });
